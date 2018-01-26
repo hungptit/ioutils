95a6504ea8a5736a6234cb6c1c5c6eefcac8b20f
Hung Dang <hdang@athenahealth.com>
2018-01-26 11:34:20 -0500

    Move boost related functinality to a new header file.

---
+++ src/boost_memmap.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <boost/iostreams/device/mapped_file.hpp>
+
+namespace ioutils {
+    // Read a file content into a buffer using memory mapped.
+    // Note: This function has a reasonable performance.
+    template <typename Container> Container read_memmap(const std::string &afile) {
+        boost::iostreams::mapped_file mmap(afile, boost::iostreams::mapped_file::readonly);
+        auto begin = mmap.const_data();
+        auto end = begin + mmap.size();
+        return Container(begin, end);
+    }
+}
