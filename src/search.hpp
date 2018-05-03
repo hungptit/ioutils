99e557f3343c1f45db22b33fd57032ecc8b5e560
Hung Dang <hdang@athenahealth.com>
2018-05-03 18:03:17 -0400

    Create a working version of mfind.

--- src/search.hpp
+++ src/search.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
 #include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <dirent.h>
+#include <fcntl.h>
 #include <set>
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <unordered_set>
-#include <fcntl.h>
-#include <ctime>
-#include <dirent.h>
-#include <stdlib.h>
 
-#include "utils/timeutils.hpp"
 #include "fmt/format.h"
+#include "utils/timeutils.hpp"
 
 // cereal
 #include "cereal/archives/binary.hpp"
@@ -30,49 +31,43 @@ namespace ioutils {
     using DefaultIArchive = cereal::BinaryInputArchive;
     using DefaultOArchive = cereal::BinaryOutputArchive;
 
-	namespace filesystem {
-		enum Error : int8_t {
-			SUCCESS = 0,
-			FAILED = -1,
-		};
-		
-		bool is_regular_file(const mode_t st_mode) {
-            return (st_mode & S_IFMT) == S_IFREG;
-        }
+    namespace filesystem {
+        enum Error : int8_t {
+            SUCCESS = 0,
+            FAILED = -1,
+        };
 
-        bool is_directory(const mode_t st_mode) {
-            return (st_mode & S_IFMT) == S_IFDIR;
-        }
+        bool is_regular_file(const mode_t st_mode) { return (st_mode & S_IFMT) == S_IFREG; }
 
-        bool is_symlink(const mode_t st_mode) {
-            return (st_mode & S_IFMT) == S_IFLNK;
-        }
+        bool is_directory(const mode_t st_mode) { return (st_mode & S_IFMT) == S_IFDIR; }
 
-		struct Utils {
-			const char *get_absolute_path(const char *p, Error &errcode) {
-				const char *results = realpath(p, fullpath);
-				errcode = (results != nullptr) ? SUCCESS : FAILED;
-				return results;
-			}
+        bool is_symlink(const mode_t st_mode) { return (st_mode & S_IFMT) == S_IFLNK; }
 
-			const char* get_current_directory(Error &errcode) {
-				const char* p = getcwd(fullpath, PATH_MAX);
-				errcode = (p != nullptr) ? SUCCESS : FAILED;
-				return p;
-			}
-			
-			char fullpath[PATH_MAX]; 
-		};
+        struct Utils {
+            const char *get_absolute_path(const char *p, Error &errcode) {
+                const char *results = realpath(p, fullpath);
+                errcode = (results != nullptr) ? SUCCESS : FAILED;
+                return results;
+            }
 
-		struct Path {
-			int fd;
-			char fullpath[1024] ;
-		};
-	}
-	
+            const char *get_current_directory(Error &errcode) {
+                const char *p = getcwd(fullpath, PATH_MAX);
+                errcode = (p != nullptr) ? SUCCESS : FAILED;
+                return p;
+            }
+
+            char fullpath[PATH_MAX];
+        };
+
+        struct Path {
+            int fd;
+            char fullpath[1024];
+        };
+    } // namespace filesystem
+
     // A struct which stores all information about a file.
     struct Stats {
-        mode_t st_mode;  /* protection */
+        mode_t st_mode; /* protection */
         // off_t st_size;   /* total size, in bytes */
         // time_t st_atime; /* time of last access */
         // time_t st_mtime; /* time of last modification */
@@ -89,20 +84,21 @@ namespace ioutils {
         std::string path;
     };
 
-	struct Path {
-		int fd;
-		std::string path;
-	};
-	
+    struct Path {
+        int fd;
+        std::string path;
+    };
+
     struct FileSearch {
         // template <typename T> DFS(T &&paths) : folders(std::forward<T>(paths)) {}
         const std::vector<Stats> &dfs(const std::vector<Path> &p) {
-			// Preprocess input arguments
-			for (auto item : p) {
-				folders.emplace_back(item);
-			}
+            // Preprocess input arguments
+            for (auto item : p) {
+                folders.emplace_back(item);
+				fmt::print("{}\n", item.path);
+            }
 
-			// Search for files and folders using DFS traversal.
+            // Search for files and folders using DFS traversal.
             while (!folders.empty()) {
                 auto parent = folders.back();
                 folders.pop_back();
@@ -112,47 +108,47 @@ namespace ioutils {
         }
 
         void visit(Path &dir) {
-			struct stat props;
-			const int fd = dir.fd;
-			int retval = fstat(fd, &props);
+            struct stat props;
+            const int fd = dir.fd;
+            int retval = fstat(fd, &props);
+            // fmt::print("Enter {}\n", dir.path);
+            if (ioutils::filesystem::is_directory(props.st_mode)) {
+                DIR *dirp = fdopendir(fd);
+                if (dirp != nullptr) {
+                    struct dirent *info;
+                    while ((info = readdir(dirp)) != NULL) {
+                        switch (info->d_type) {
+                        case DT_DIR:
+                            if ((strcmp(info->d_name, ".") != 0) &&
+                                (strcmp(info->d_name, "..") != 0) &&
+                                (strcmp(info->d_name, ".git") != 0)) {
+                                Path current_dir = dir;
+                                current_dir.path.push_back('/');
+                                current_dir.path.append(info->d_name);
+                                current_dir.fd = ::open(current_dir.path.data(), O_RDONLY);
+                                fmt::print("{0}\n", current_dir.path);
+                                folders.emplace_back(current_dir);
+                            }
+                            break;
+                        case DT_REG:
+                            fmt::print("{0}/{1}\n", dir.path, info->d_name);
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                    (void)closedir(dirp);
+                }
+            } else {
+                fmt::print("How can we get here?\n");
+            }
+            ::close(fd);
+        }
 
-			if (ioutils::filesystem::is_directory(props.st_mode)) {
-				DIR* dirp = fdopendir(fd);
-				if (dirp != nullptr) {
-					struct dirent *info;
-					while ((info = readdir(dirp)) != NULL) {
-						// How to get a
-						switch (info->d_type) {
-						case DT_DIR:
-							if ((strncmp(info->d_name, ".", info->d_namlen) != 0) && (strncmp(info->d_name, "..", info->d_namlen) != 0)) {
-								Path current_dir = dir;
-								current_dir.path.push_back('/');
-								current_dir.path.append(info->d_name, info->d_namlen);
-								current_dir.fd = ::open(current_dir.path.data(), O_RDONLY);
-								fmt::print("{0}\n", current_dir.path);
-								folders.emplace_back(current_dir);
-							}
-							break;
-						case DT_REG:
-							fmt::print("{0}/{1}\n", dir.path, info->d_name);
-							break;
-						default:
-							break;
-						}
-
-					}
-					(void)closedir(dirp);
-				}
-			} else if (ioutils::filesystem::is_regular_file(props.st_mode)) {
-				fmt::print("File : {0}\n", props.st_size);
-			} else if (ioutils::filesystem::is_symlink(props.st_mode)) {
-				fmt::print("Got a symlink\n");
-			} else {
-				fmt::print("How can we get here?\n");
-			}
-			::close(fd);		
+		void process_file(const Path &dir, const struct dirent* info ) {
+			
 		}
-
+		
         std::unordered_set<std::string> status;
         std::vector<Stats> files;
         std::vector<Path> folders;
