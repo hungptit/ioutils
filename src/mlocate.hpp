1a13c4cf36a850d2b64825a020294221c66afe10
Hung Dang <hungptit@gmail.com>
2018-05-19 23:42:41 -0400

    Add a header which implements the mlocate functionality.

---
+++ src/mlocate.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include "search.hpp"
+#include "filesystem.hpp"
+
+namespace utils {
+    struct MlocatePolicy {
+
+    };
+
+// Write search data to database.
+    struct MlocateDBWriter {};
+
+    // Read mlocate database
+    struct MlocateDBReader {};
+
+}
