933514231b71a9bc920b5aa8929a4d329c8e7ed6
Hung Dang [hdang] <hdang@athenahealth.com>
2018-05-23 14:29:27 -0400

    Commit local changes.

--- command/mfind.cpp
+++ command/mfind.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     for (auto idx = argc - 1; idx > 0; --idx) {
         p.emplace_back(argv[idx]);
         ioutils::FileSearch search;
-        search.bfs(p);
+        search.dfs(p);
     }
     return EXIT_SUCCESS;
 }
