d8db888f94d19b9bf1a44e9bddbec2a8f1dcf363
Hung Dang <hungptit@gmail.com>
2018-05-27 22:01:26 -0400

    Fixing performance issue in the latest kernel.

--- benchmark/file_read.cpp
+++ benchmark/file_read.cpp
@@ -3,18 +3,34 @@
 #include <fcntl.h>
 #include <sstream>
 #include <string>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <vector>
 
+#include "boost/coroutine2/all.hpp"
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+#include "ioutils.hpp"
+#include "linestats.hpp"
+
+#include "celero/Celero.h"
+
 namespace {
+    constexpr char EOL('\n');
     // Read file using memory mapped approach.
-    template <typename Container> Container read_memmap(const std::string &afile) {
+    size_t read_memmap(const std::string &afile) {
         boost::iostreams::mapped_file mmap(afile, boost::iostreams::mapped_file::readonly);
         auto begin = mmap.const_data();
         auto end = begin + mmap.size();
-        return Container(begin, end);
+        size_t counter = 0;
+        for (auto iter = begin; iter != end; ++iter) {
+            counter += *iter == EOL;
+        }
+        return counter;
     }
 
     // Read from file in chunks
@@ -30,12 +46,12 @@ namespace {
     constexpr size_t READ_TRUNK_SIZE = 1 << 16;
 
     // A reader class which stores the policy as a member data.
-    template <typename Policy, size_t BUFFER_SIZE = READ_TRUNK_SIZE>
-    struct FileReader : public Policy {
-        FileReader() : Policy() {}
-        FileReader(const std::string &pattern) : Policy(pattern) {}
+    std::atomic<bool> is_stop(false);
+    template <int BUFFER_SIZE> struct FileReader {
+        char process_buffer[BUFFER_SIZE + 1];
 
-        void operator()(const char *datafile) {
+        size_t operator()(const char *datafile) { return read(datafile); }
+        size_t read(const char *datafile) {
             char read_buffer[BUFFER_SIZE + 1];
             int fd = ::open(datafile, O_RDONLY | O_NOCTTY);
 
@@ -46,11 +62,15 @@ namespace {
                 throw(std::runtime_error(writer.str()));
             }
 
-            // Let the kernel know that we are going to read sequentially to the end of a file.
-            // posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
+            // Get file size.
+            struct stat buf;
+            fstat(fd, &buf);
 
             // Read data into a read buffer
-            while (long nbytes = ::read(fd, read_buffer, BUFFER_SIZE)) {
+            size_t block_count = (buf.st_size / BUFFER_SIZE) + (buf.st_size % BUFFER_SIZE != 0);
+            size_t counter = 0;
+            for (size_t blk = 0; blk < block_count; ++blk) {
+                long nbytes = ::read(fd, read_buffer, BUFFER_SIZE);
                 if (nbytes < 0) {
                     std::stringstream writer;
                     writer << "Cannot read file \"" << datafile << "\" "
@@ -58,36 +78,106 @@ namespace {
                     throw(std::runtime_error(writer.str()));
                 };
 
-                // Apply a given policy to read_buffer.
-                Policy::process(read_buffer, nbytes);
-
-                // Stop if we reach the end of file.
-                if (nbytes != static_cast<decltype(nbytes)>(BUFFER_SIZE)) {
-                    break;
-                };
+                memcpy(process_buffer, read_buffer, nbytes);
+                counter += process(read_buffer, nbytes);
             }
 
             // Close our file.
             ::close(fd);
+
+            return counter;
+        }
+
+        size_t process(const char *buffer, long nbytes) {
+            size_t counter = 0;
+            const char *end = buffer + nbytes;
+            const char *ptr = buffer;
+            while ((ptr = static_cast<const char *>(memchr(ptr, EOL, end - ptr)))) {
+                ++counter;
+                ++ptr;
+            }
+            // for (auto idx = 0; idx < nbytes; ++idx) {
+            //     counter += buffer[idx] == EOL;
+            // }
+            return counter;
         }
     };
-
 } // namespace
 
-int main(const int argc, char *argv[]) {
-    if (argc < 3) {
-        fmt::print("Need to provide an option and a file name\n");
-    }
+const int number_of_samples = 10;
+const int number_of_operations = 2;
+const std::string fname("3200.txt");
 
-    std::string option(argv[1]);
-    std::string fname(argv[2]);
-    if (option == "boost") {
-        auto data = read_memmap<std::vector<char>>(fname);
-        fmt::print("Number of bytes: {}\n", data.size());
-    } else if (option == "chunk") {
-        using Reader = FileReader<AppendPolicy, 1 << 16>;
-        Reader reader;
-        reader(fname.c_str());
-        fmt::print("Number of bytes: {}\n", reader.data().size());
-    }
+// Use stat function
+// void use_boost(benchmark::State &state) {
+//     for (auto _ : state) {
+//         benchmark::DoNotOptimize(read_memmap(fname));
+//     }
+// }
+
+// // Use stat function
+// void read_chunk(benchmark::State &state) {
+//     for (auto _ : state) {
+//         using Reader = FileReader<1 << 16>;
+//         FileReader<1 << 16> reader;
+//         reader(fname.c_str());
+//     }
+// }
+
+// // Use stat function
+// void read_ioutils(benchmark::State &state) {
+//     for (auto _ : state) {
+//         using FastLineStats = ioutils::FileReader<ioutils::LineStats_std, 1 << 16>;
+//         FastLineStats linestats;
+//         linestats(fname.c_str());
+//     }
+// }
+
+// BENCHMARK(read_chunk);
+// BENCHMARK(use_boost);
+// BENCHMARK(read_ioutils);
+// BENCHMARK_MAIN();
+
+CELERO_MAIN
+
+BASELINE(read, boost_memmap, number_of_samples, number_of_operations) {
+    celero::DoNotOptimizeAway(read_memmap(fname.c_str()));
 }
+
+BENCHMARK(read, read_chunk, number_of_samples, number_of_operations) {
+    FileReader<1 << 16> reader;
+    reader(fname.c_str());
+}
+
+BENCHMARK(read, ioutils, number_of_samples, number_of_operations) {
+    using FastLineStats = ioutils::FileReader<ioutils::LineStats_std, 1 << 16>;
+    FastLineStats linestats;
+    linestats(fname.c_str());
+}
+
+BENCHMARK(read, ioutils_memchr, number_of_samples, number_of_operations) {
+    using FastLineStats = ioutils::FileReader<ioutils::LineStats, 1 << 16>;
+    FastLineStats linestats;
+    linestats(fname.c_str());
+}
+
+// int main(const int argc, char *argv[]) {
+//     if (argc < 3) {
+//         fmt::print("Need to provide an option and a file name\n");
+//     }
+
+//     std::string option(argv[1]);
+//     std::string fname(argv[2]);
+//     if (option == "boost") {
+//         fmt::print("Number of lines: {}\n", read_memmap(fname));
+//     } else if (option == "chunk") {
+//         using Reader = FileReader<1 << 16>;
+//         Reader reader;
+//         fmt::print("Number of lines: {}\n", reader(fname.c_str()));
+//     } else {
+//         using FastLineStats = ioutils::FileReader<ioutils::LineStats_std, 1 << 16>;
+//         FastLineStats linestats;
+//         linestats(fname.c_str());
+//         fmt::print("Number of lines: {}\n", linestats.lines);
+//     }
+// }
