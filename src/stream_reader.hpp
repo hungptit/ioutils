edb4df2859973558ab453cf6b1b98a626d23bd2d
Hung Dang <hdang@athenahealth.com>
2018-12-07 00:07:16 -0500

    Add a simple stream reader.

---
+++ src/stream_reader.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <unistd.h>
+
+namespace ioutils {
+    template <typename Policy, size_t BUFFER_SIZE = 1 << 16>
+    struct StreamReader : public Policy {
+        template <typename... Args>
+        StreamReader(Args... args) : Policy(std::forward<Args>(args)...) {}
+        void operator()(int fd) {
+            char read_buffer[BUFFER_SIZE + 1];
+            while (true) {
+                long nbytes = ::read(fd, read_buffer, BUFFER_SIZE);
+                if (nbytes < 1) break;
+                Policy::process(read_buffer, nbytes);
+                if (nbytes != BUFFER_SIZE) break;
+            }
+        }
+    };
+} // namespace ioutils
