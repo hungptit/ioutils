1c75ef4e3379031133b6cff1cd027755fd939155
Hung Dang <hdang@athenahealth.com>
2018-12-12 16:07:18 -0500

    Fix a typo in MMapReader's comment.

--- src/reader.hpp
+++ src/reader.hpp
@@ -48,7 +48,7 @@ namespace ioutils {
         }
     };
 
-    // A breader that use memory mapped approach.
+    // A reader that use memory mapped approach.
     template <typename Policy> struct MMapReader : public Policy {
         MMapReader() : Policy() {}
 
