29a948167730cdea1eff6749b1f2634b1d901d8d
Hung Dang <hdang@athenahealth.com>
2019-01-10 00:22:33 -0500

    Add regex_store_policies.hpp

---
+++ src/regex_store_policies.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "filesystem.hpp"
+#include "fmt/format.h"
+#include "resources.hpp"
+#include "utils/regex_matchers.hpp"
+#include <string>
+
+namespace ioutils {
+    template <typename Matcher> class RegexStorePolicy {
+      public:
+        template <typename Params>
+        RegexStorePolicy(Params &&params)
+            : buffer(), matcher(params.path_regex, params.regex_mode), files(),
+              store_file(!params.ignore_file()), store_dir(!params.ignore_dir()),
+              store_symlink(!params.ignore_symlink()) {
+            buffer.reserve(1023);
+        }
+
+        const std::vector<std::string> &get_files() const { return files; }
+
+      protected:
+        bool is_valid_dir(const char *dname) const { return filesystem::is_valid_dir(dname); }
+
+        void process_file(const std::string &parent, const char *stem) {
+            if (!store_file) return;
+            buffer = parent + "/" + stem;
+            if (matcher.is_matched(buffer.data(), buffer.size())) {
+                files.push_back(buffer);
+            }
+        }
+
+        void process_symlink(const std::string &parent, const char *stem) {
+            if (!store_symlink) return;
+            buffer = parent + "/" + stem;
+            if (matcher.is_matched(buffer.data(), buffer.size())) {
+                files.push_back(buffer);
+            }
+        }
+
+        void process_dir(const std::string &p) {}
+
+        std::string buffer;
+        Matcher matcher;
+        std::vector<std::string> paths;
+
+        bool store_file = false;
+        bool store_dir = false;
+        bool store_symlink = false;
+    };
+} // namespace ioutils
