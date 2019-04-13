dbf2be8d602d05c4f22b893025202b8e466bb525
Hung Dang <hdang@athenahealth.com>
2019-04-13 12:48:23 -0400

    Update the setup script for fast-locate command.

--- benchmark/setup.sh
+++ benchmark/setup.sh
@@ -3,9 +3,9 @@ set -e
 search_path="${HOME}/working/3p/src/"
 
 # Create an indexed database for fast-locate
-fast-locate_db=".database"
-rm -f $fast-locate_db
-mupdatedb $search_path -d "$fast-locate_db"
+fast_locate_db=".database"
+rm -f $fast_locate_db
+mupdatedb $search_path -d "$fast_locate_db"
 
 # Create an indexed database for GNU locate
 locate_db="locate.db"
