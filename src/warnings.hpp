821e0701d6ecd2bf962db51341c606e78c94681c
Hung Dang <hdang@athenahealth.com>
2019-04-22 10:42:12 -0400

    Add a template function to suppress the compiler warnings about unused values.

---
+++ src/warnings.hpp
@@ -0,0 +1,5 @@
+#pragma once
+namespace ioutils {
+    // https://herbsutter.com/2009/10/18/mailbag-shutting-up-compiler-warnings/
+    template <class T> void ignore(const T &) {}
+} // namespace ioutils
