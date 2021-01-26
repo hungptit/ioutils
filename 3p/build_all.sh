20feaafe8abfc588bd68a51e88a82e04443386d1
Hung Dang <hungptit@gmail.com>
2021-01-25 19:05:42 -0500

    Initial commit

--- 3p/build_all.sh
+++ 3p/build_all.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 set -e                          # Use Bash strict mode
 
-printf "Build doctest\n";
-# ./build_using_cmake.sh doctest "-DCMAKE_CXX_COMPILER=clang++" > /dev/null
-cp -rv src/doctest/doctest include/
-
-printf "Build FMT\n"
-./build_using_cmake.sh fmt -DFMT_TEST=OFF > /dev/null
-
-printf "Build benchmark\n"
-./build_using_cmake.sh benchmark -DBENCHMARK_ENABLE_TESTING=OFF > /dev/null
-
-printf "Setup Clara\n"
-cp src/Clara/include/*.hpp include/
-
-printf "Build Celero\n"
-./build_using_cmake.sh Celero "-DCELERO_COMPILE_DYNAMIC_LIBRARIES:BOOL=OFF -DCELERO_ENABLE_EXPERIMENTS:BOOL=OFF" > /dev/null
-
-printf "Build utils"
-pushd src/utils/
-./install.sh ../../
-popd
-
 printf "Build hyperscan"
 ./build_using_cmake.sh hyperscan
