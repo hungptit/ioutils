3f758dcafb9c659badfe020ef81bcee1fa6c4f2e
Hung Dang <hungptit@gmail.com>
2021-02-27 16:50:56 -0500

    Able to compile hyperscan using fetchcontent, however, avx2 option is disable.

--- build_hyperscan.sh
+++ build_hyperscan.sh
@@ -8,7 +8,8 @@ rm -rf $tmp_dir
 mkdir -p $tmp_dir
 
 pushd "$tmp_dir" || exit
-cmake "$src_dir" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$root_dir/3p" -DHAVE_AVX2=TRUE -DBUILD_EXAMPLES=OFF "$@"
+# Disable FAT_RUNTIME for local build.
+cmake "$src_dir" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$root_dir/3p" -DFAT_RUNTIME=OFF -DHAVE_AVX2=TRUE -DBUILD_EXAMPLES=OFF "$@"
 make -j8
 make install
 popd || exit
